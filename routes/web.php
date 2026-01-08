44c97fc8080178d968a3ef82e308a24ded0c0485
skgrendel <bryan.andion@gmail.com>
2026-01-08 13:45:45 -0500

    Eliminar vistas de bienvenida y confirmación de pago

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-/**
- * Página de confirmación de pago
- */
-Route::get('/pago/confirmacion', function () {
-    return view('payment-success');
-});
-
-
 
